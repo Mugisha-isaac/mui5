5d9e58c359ae048ebd016889bfac3af1d7fdf93b
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-06-04 08:58:24 +0200

    wip: toolbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { AppBar,Toolbar,styled,Typography } from '@mui/material'
+import PetsIcon from '@mui/icons-material/Pets';
+
 
 const Navbar = () => {
+
+const StyledToolbar = styled(Toolbar)({
+    display:"flex",
+    justifyContent:"space-between"
+})
+
+
+
+
   return (
-    <div>Navbar</div>
-  )
+    <AppBar position='sticky'>
+      <StyledToolbar>
+          <Typography  variant='h6' sx={{display: {xs:"none", sm:"block" }}}>MUI DEV</Typography>
+          <PetsIcon sx={{display: {xs:"block", sm:"none" }}} />
+      </StyledToolbar>
+    </AppBar>
+  ) 
 }
 
 export default Navbar
