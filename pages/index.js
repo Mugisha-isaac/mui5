2fdc0926e63fd1e3e16291f80fb780dad66dcfb0
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-06-04 06:49:50 +0200

    feat: buttons and typography

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
+import { Button, Typography } from '@mui/material'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export default function Home() {
   return (
    <div>
-     hello
+      <Button variant="text">Text</Button>
+      <Button variant="contained" startIcon={<DeleteIcon />} color='success' size='small'>Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+      <Typography variant='h4' component='p'>
+          this is typography 
+      </Typography>
    </div>
   )
 }
