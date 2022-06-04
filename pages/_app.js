aded6f1ca91536cb7ac92461316393600ae09a30
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-06-04 07:09:26 +0200

    wip:defining custom themes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import { ThemeProvider } from '@mui/material'
 import '../styles/globals.css'
+import { theme } from '../themes/theme'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,31 @@
-import { Button, Typography } from '@mui/material'
+import { Button, styled, Typography } from '@mui/material'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import DeleteIcon from '@mui/icons-material/Delete';
 
 export default function Home() {
+
+  const BlueButton = styled(Button)({
+    backgroundColor:'skyblue', color:'#888', m:5,
+       "&:hover":{
+        backgroundColor:'lightblue'
+      },
+      "&:disabled":{
+        backgroundColor:'gray',
+        color:"white"
+      }
+  })
+
+
   return (
    <div>
       <Button variant="text">Text</Button>
       <Button variant="contained" startIcon={<DeleteIcon />} color='success' size='small'>Contained</Button>
       <Button variant="outlined">Outlined</Button>
-      <Typography variant='h4' component='p'>
+      <BlueButton>My button</BlueButton>
+      <BlueButton>Another button</BlueButton>
+      <Typography variant='h4' component='p' >
           this is typography 
       </Typography>
    </div>
