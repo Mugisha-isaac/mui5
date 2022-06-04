e218501d79ebc5412d792ceb93381fcc235b74a0
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-06-04 10:40:57 +0200

    wip: List and card

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,9 +1,50 @@
-import { Box } from '@mui/material'
+import { Box,Card,CardHeader,CardMedia,CardContent,CardActions,Avatar,IconButton,Typography } from '@mui/material'
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 import React from 'react'
 
 const Feed = () => {
   return (
-    <Box bgcolor="pink" flex={4}  p={2} >Feed</Box>
+    <Box  flex={4}  p={2}>
+    <Card>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: "red" }} aria-label="recipe">
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="Shrimp and Chorizo Paella"
+        subheader="September 14, 2016"
+      />
+      <CardMedia
+        component="img"
+        height="20%"
+        image="https://www.foxandbriar.com/wp-content/uploads/2016/04/Shrimp-and-Chorizo-Paella-6-of-10.jpg"
+        alt="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          This impressive paella is a perfect party dish and a fun meal to cook
+          together with your guests. Add 1 cup of frozen peas along with the mussels,
+          if you like.
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+      </CardActions>
+    </Card>
+      </Box>
   )
 }
 
--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,10 +1,84 @@
-import { Box } from "@mui/material";
 import React from "react";
+import { Box,List,ListItem,ListItemButton,ListItemIcon,ListItemText,Switch } from "@mui/material";
+import HomeIcon from '@mui/icons-material/Home';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import PersonIcon from '@mui/icons-material/Person';
+import SettingsApplicationsIcon from '@mui/icons-material/SettingsApplications';
+import ModeNightIcon from '@mui/icons-material/ModeNight';
+
 
 const Sidebar = () => {
   return (
-    <Box bgcolor="skyblue" flex={1} p={2} sx={{display:{xs:"none", sm:"block"}}}>
-      Sidebar
+    <Box flex={1} p={2} sx={{display:{xs:"none", sm:"block"}}}>
+     <List>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#home">
+              <ListItemIcon>
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText primary="Homepage" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemIcon>
+                <AutoStoriesIcon />
+              </ListItemIcon>
+              <ListItemText primary="Pages" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemIcon>
+                <PeopleAltIcon />
+              </ListItemIcon>
+              <ListItemText primary="Groups" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemIcon>
+                <StorefrontIcon />
+              </ListItemIcon>
+              <ListItemText primary="Marketplace" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemIcon>
+                <PersonIcon />
+              </ListItemIcon>
+              <ListItemText primary="Friends" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemIcon>
+                <SettingsApplicationsIcon />
+              </ListItemIcon>
+              <ListItemText primary="Settings" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemIcon>
+                <AccountBoxIcon />
+              </ListItemIcon>
+              <ListItemText primary="Profile" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemIcon>
+                <ModeNightIcon />
+              </ListItemIcon>
+              <Switch  defaultChecked />
+            </ListItemButton>
+          </ListItem>
+    </List>
     </Box>
   );
 };
