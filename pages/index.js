cc46836cd6809f266878c6eb181a923937ae73b4
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-06-04 08:45:34 +0200

    wip: responsiveness

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,14 @@ import Sidebar from '../components/Sidebar';
 import Feed from '../components/Feed';
 import Rightbar from '../components/Rightbar';
 import { Box,Stack } from '@mui/material';
+import Navbar from '../components/Navbar';
 
 export default function Home() {
 
 
   return (
    <Box>
-     {/* navbar */}
-     
+     <Navbar/>
      <Stack direction="row" spacing={2} justifyContent="space-between">
      <Sidebar />
      <Feed />
